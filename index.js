becc63307eb18634096fe4581b19eaf0a418185e
Jack Jennings <jack@standard-library.com>
2017-05-22 13:07:08 -0700

    Import Kefir.merge directly as mergeStreams

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { Kefir as K } from 'kefir';
+import { merge as mergeStreams } from 'kefir';
 import { toPairs, map, assoc, compose } from 'ramda';
 
 const patches = ([name, values]) => values.map(assoc(name));
@@ -7,7 +7,7 @@ const patchBy = compose(map(patches), toPairs);
 const evolve = (blueprint, initial) => {
   const transformations = patchBy(blueprint);
 
-  return K.merge(transformations).scan((c, t) => t(c), initial);
+  return mergeStreams(transformations).scan((c, t) => t(c), initial);
 }
 
 export default evolve;
