21032ab1c3e011364a8942a3717cdc3f2b4f5a79
Jack Jennings <jack@standard-library.com>
2018-02-11 13:31:57 -0800

    Remove ramda as a dependency

--- build/index.js
+++ build/index.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 Object.defineProperty(exports, "__esModule", {
   value: true
@@ -6,23 +6,51 @@ Object.defineProperty(exports, "__esModule", {
 
 var _slicedToArray = function () { function sliceIterator(arr, i) { var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"]) _i["return"](); } finally { if (_d) throw _e; } } return _arr; } return function (arr, i) { if (Array.isArray(arr)) { return arr; } else if (Symbol.iterator in Object(arr)) { return sliceIterator(arr, i); } else { throw new TypeError("Invalid attempt to destructure non-iterable instance"); } }; }();
 
-var _kefir = require('kefir');
+var _kefir = require("kefir");
 
-var _ramda = require('ramda');
+var toPairs = function toPairs(obj) {
+  var ownProps = Object.keys(obj);
+  var i = ownProps.length;
+  var result = new Array(i); // preallocate the Array
 
-var patches = function patches(_ref) {
+  while (i--) {
+    result[i] = [ownProps[i], obj[ownProps[i]]];
+  }
+
+  return result;
+};
+
+var assoc = function assoc(prop, val, obj) {
+  var result = {};
+
+  for (var p in obj) {
+    result[p] = obj[p];
+  }
+
+  result[prop] = val;
+
+  return result;
+};
+
+var pairToPatch = function pairToPatch(_ref) {
   var _ref2 = _slicedToArray(_ref, 2),
       name = _ref2[0],
       values = _ref2[1];
 
-  return values.map((0, _ramda.assoc)(name));
+  return values.map(function (v) {
+    return function (b) {
+      return assoc(name, v, b);
+    };
+  });
 };
-var patchBy = (0, _ramda.compose)((0, _ramda.map)(patches), _ramda.toPairs);
+var patchBy = function patchBy(obj) {
+  return toPairs(obj).map(pairToPatch);
+};
 
 var evolve = function evolve(blueprint, initial) {
   var transformations = patchBy(blueprint);
 
-  return _kefir.Kefir.merge(transformations).scan(function (c, t) {
+  return (0, _kefir.merge)(transformations).scan(function (c, t) {
     return t(c);
   }, initial);
 };
--- index.js
+++ index.js
@@ -1,13 +1,36 @@
-import { merge as mergeStreams } from 'kefir';
-import { toPairs, map, assoc, compose } from 'ramda';
+import { merge as mergeStreams } from "kefir";
 
-const patches = ([name, values]) => values.map(assoc(name));
-const patchBy = compose(map(patches), toPairs);
+const toPairs = function(obj) {
+  var ownProps = Object.keys(obj);
+  var i = ownProps.length;
+  var result = new Array(i); // preallocate the Array
+
+  while (i--) {
+    result[i] = [ownProps[i], obj[ownProps[i]]];
+  }
+
+  return result;
+};
+
+const assoc = (prop, val, obj) => {
+  var result = {};
+
+  for (var p in obj) {
+    result[p] = obj[p];
+  }
+
+  result[prop] = val;
+
+  return result;
+};
+
+const pairToPatch = ([name, values]) => values.map(v => b => assoc(name, v, b));
+const patchBy = obj => toPairs(obj).map(pairToPatch);
 
 const evolve = (blueprint, initial) => {
   const transformations = patchBy(blueprint);
 
   return mergeStreams(transformations).scan((c, t) => t(c), initial);
-}
+};
 
 export default evolve;
